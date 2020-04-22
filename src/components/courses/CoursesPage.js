2807ca09aa4beb54ccd60104ebe26411a39fd051
SunilPrakash <sunil.prakash@talentica.com>
2020-04-22 17:33:50 +0530

    Hide empty course list

--- src/components/courses/CoursesPage.js
+++ src/components/courses/CoursesPage.js
@@ -11,26 +11,26 @@ import { toast } from "react-toastify";
 
 class CoursesPage extends React.Component {
   state = {
-    redirectToAddCoursePage: false
+    redirectToAddCoursePage: false,
   };
 
   componentDidMount() {
     const { courses, authors, actions } = this.props;
 
     if (courses.length === 0) {
-      actions.loadCourses().catch(error => {
+      actions.loadCourses().catch((error) => {
         alert("Loading courses failed" + error);
       });
     }
 
     if (authors.length === 0) {
-      actions.loadAuthors().catch(error => {
+      actions.loadAuthors().catch((error) => {
         alert("Loading authors failed" + error);
       });
     }
   }
 
-  handleDeleteCourse = async course => {
+  handleDeleteCourse = async (course) => {
     toast.success("Course deleted");
     try {
       await this.props.actions.deleteCourse(course);
@@ -56,10 +56,14 @@ class CoursesPage extends React.Component {
               Add Course
             </button>
 
-            <CourseList
-              onDeleteClick={this.handleDeleteCourse}
-              courses={this.props.courses}
-            />
+            {this.props.courses.length > 0 ? (
+              <CourseList
+                onDeleteClick={this.handleDeleteCourse}
+                courses={this.props.courses}
+              />
+            ) : (
+              <h3>No Courses to Display</h3>
+            )}
           </>
         )}
       </>
@@ -71,7 +75,7 @@ CoursesPage.propTypes = {
   authors: PropTypes.array.isRequired,
   courses: PropTypes.array.isRequired,
   actions: PropTypes.object.isRequired,
-  loading: PropTypes.bool.isRequired
+  loading: PropTypes.bool.isRequired,
 };
 
 function mapStateToProps(state) {
@@ -79,14 +83,15 @@ function mapStateToProps(state) {
     courses:
       state.authors.length === 0
         ? []
-        : state.courses.map(course => {
+        : state.courses.map((course) => {
             return {
               ...course,
-              authorName: state.authors.find(a => a.id === course.authorId).name
+              authorName: state.authors.find((a) => a.id === course.authorId)
+                .name,
             };
           }),
     authors: state.authors,
-    loading: state.apiCallsInProgress > 0
+    loading: state.apiCallsInProgress > 0,
   };
 }
 
@@ -95,12 +100,9 @@ function mapDispatchToProps(dispatch) {
     actions: {
       loadCourses: bindActionCreators(courseActions.loadCourses, dispatch),
       loadAuthors: bindActionCreators(authorActions.loadAuthors, dispatch),
-      deleteCourse: bindActionCreators(courseActions.deleteCourse, dispatch)
-    }
+      deleteCourse: bindActionCreators(courseActions.deleteCourse, dispatch),
+    },
   };
 }
 
-export default connect(
-  mapStateToProps,
-  mapDispatchToProps
-)(CoursesPage);
+export default connect(mapStateToProps, mapDispatchToProps)(CoursesPage);
